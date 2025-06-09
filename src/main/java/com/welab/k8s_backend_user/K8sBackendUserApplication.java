aee328ab2dcfd4c8c5ae43e43b33f76761ec1f36
wkdnffla3 <wkdnffla3@gmail.com>
2025-06-09 15:27:36 +0900

    first commit

---
+++ src/main/java/com/welab/k8s_backend_user/K8sBackendUserApplication.java
@@ -0,0 +1,13 @@
+package com.welab.k8s_backend_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sBackendUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sBackendUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/welab/k8s_backend_user/K8sBackendUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welab.k8s_backend_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sBackendUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
