4fb1661e324908577478f1300b3e244cd154a2f1
wkdnffla3 <wkdnffla3@gmail.com>
2025-06-11 16:59:56 +0900

    asd

--- src/main/java/com/welab/k8s_backend_user/K8sBackendUserApplication.java
+++ src/main/java/com/welab/k8s_backend_user/K8sBackendUserApplication.java
@@ -2,7 +2,9 @@ package com.welab.k8s_backend_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class K8sBackendUserApplication {
 
